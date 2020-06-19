b291660abaa907758903d1a2ff22781c35c3481d
Aaron Bronow <aaron@aaronbronow.com>
2020-06-19 16:33:45 -0700

    add debugging

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,13 @@
 class ApiController < ApplicationController
   def index
+    puts params
     agent_status = AgentStatus.create(
       details: params[:host], 
       version: params[:v], 
       agent_time: DateTime.strptime(params[:time],'%s'), 
       package_manager: params[:pkgmgr], 
       message: params[:msg])
+    agent_status.save
     respond_to do |format|
       format.json { render json: "{'got it'}" }
     end
