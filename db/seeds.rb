9f4b03cab14231922dd96ba9f149531c8cdc0f72
Aaron Bronow <abronow@gmail.com>
2024-03-02 09:47:12 -0800

    prep user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: ENV['SEED_USER_EMAIL'], password: ENV['SEED_USER_PASS'])
